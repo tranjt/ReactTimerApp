b960351401352f49d05b1f780d362b69b34966e0
JTTran <tranjt80@gmail.com>
2017-05-31 22:33:26 +0300

    Create clock component

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
            Navigation: "app/components/Navigation.jsx", 
            Timer: "app/components/Timer.jsx", 
            Countdown: "app/components/Countdown.jsx", 
+           Clock: "app/components/Clock.jsx", 
            applicationStyle: "app/styles/app.css",      
         
         },        
