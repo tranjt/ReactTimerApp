74b11b0627afa15109a5dcb159ffabb1e46f3fa1
JTTran <tranjt80@gmail.com>
2017-06-01 16:50:33 +0300

    Created timer component

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,10 +1,63 @@
 var React = require("react");
+var Clock = require("Clock");
+var Control = require("Controls");
 
 var Timer = React.createClass({
+
+    getInitialState: function() {
+        return {
+            count: 0,
+            timerStatus: "stopped"
+        }
+    },
+    componentDidUpdate: function(prevProps, prevStatus) {
+        if (this.state.timerStatus !== prevStatus.timerStatus) {
+            switch(this.state.timerStatus) {
+                case "started":
+                    this.startTimer();                    
+                    break;
+                case "paused":
+                    clearInterval(this.timer);
+                    this.timer = undefined;
+                    break;
+                case "stopped":
+                    clearInterval(this.timer);
+                    this.timer = undefined;
+                    this.setState({
+                        count: 0,                       
+                    });
+                    break;
+            }
+        }
+
+    },
+    componentWillUnmount: function() {        
+        clearInterval(this.timer);
+        this.timer = undefined;
+    },
+
+    startTimer: function() {
+        this.timer = setInterval(()=>{
+            var newCount = this.state.count + 1;
+            this.setState({
+                count: newCount
+            }); 
+        }, 1000);
+    },
+
+    handleStatusChange: function(newStatus) {
+        this.setState({
+            timerStatus: newStatus
+        });
+    },
     render: function() {
+        var {count, timerStatus} = this.state;
         return (
-            <div>
-                From Timer.jsx
+            <div >
+                 <h1 className="page-title">Timer App </h1>
+                <Clock totalSeconds={count}/>
+                <Control countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
+            
             </div>
         );
     }
